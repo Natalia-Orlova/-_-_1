41896c50fa28435f040927876c0cf76c4d52c046
NataliaOrlova <natakarelskaya@yandex.ru>
2022-09-15 20:16:35 +0400

    added task solution

--- task0/Program.cs
+++ task0/Program.cs
@@ -7,3 +7,37 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] arr1 = { "hello", "its", "final", "task", "for", "1st", "quarter", "=)" };
+string[] newarray = new string[arr1.Length];
+
+int j = 0;
+for (int i = 0; i < arr1.Length; i++)
+{
+    if (arr1[i].Length <= 3)
+    {
+        newarray[j] = arr1[i];
+        j++;
+    }
+}
+
+string[] arr2 = new string[j];
+for (int k = 0; k < j; k++)
+{
+    arr2[k] = newarray[k];
+}
+
+
+void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]} ]");
+}
+
+PrintArray(arr1);
+Console.WriteLine();
+PrintArray(arr2);
